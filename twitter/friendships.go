75ce1aea7056f1a56a71ead84a9c886d2687ba4c
yyoshiki41 <yyoshiki41@gmail.com>
2017-01-24 09:17:24 +0900

    Add friendships/show

--- twitter/friendships.go
+++ twitter/friendships.go
@@ -53,3 +53,58 @@ func (s *FriendshipService) Destroy(params *FriendshipDestroyParams) (*User, *ht
 	resp, err := s.sling.New().Post("destroy.json").QueryStruct(params).Receive(user, apiError)
 	return user, resp, relevantError(err, *apiError)
 }
+
+// FriendshipShowParams are paramenters for FriendshipService.Show
+type FriendshipShowParams struct {
+	SourceID         int64  `url:"source_id,omitempty"`
+	SourceScreenName string `url:"source_screen_name,omitempty"`
+	TargetID         int64  `url:"target_id,omitempty"`
+	TargetScreenName string `url:"target_screen_name,omitempty"`
+}
+
+// Show shows a relationship the specified user and
+// returns the unfollowed user.
+// Requires a user auth or an app context.
+// https://dev.twitter.com/rest/reference/get/friendships/show
+func (s *FriendshipService) Show(params *FriendshipShowParams) (*RelationshipResp, *http.Response, error) {
+	relationship := new(RelationshipResp)
+	apiError := new(APIError)
+	resp, err := s.sling.New().Get("show.json").QueryStruct(params).Receive(relationship, apiError)
+	return relationship, resp, relevantError(err, *apiError)
+}
+
+// RelationshipResp is struct
+type RelationshipResp struct {
+	Relationship `json:"relationship"`
+}
+
+// Relationship is struct
+type Relationship struct {
+	Target *Target `json:"target"`
+	Source *Source `json:"source"`
+}
+
+// Target is struct
+type Target struct {
+	IDStr       string `json:"id_str"`
+	ID          int64  `json:"id"`
+	ScreenName  string `json:"screen_name"`
+	Following   bool   `json:"following"`
+	FollowingBy bool   `json:"followed_by"`
+}
+
+// Source is struct
+type Source struct {
+	CanDm                bool        `json:"can_dm"`
+	Blocking             interface{} `json:"blocking"`
+	Muting               interface{} `json:"muting"`
+	AllReplies           interface{} `json:"all_replies"`
+	WantRetweets         interface{} `json:"want_retweets"`
+	ID                   int64       `json:"id"`
+	IDStr                string      `json:"id_str"`
+	MarkedSpam           interface{} `json:"marked_spam"`
+	ScreenName           string      `json:"screen_name"`
+	Following            bool        `json:"following"`
+	FollowedBy           bool        `json:"followed_by"`
+	NotificationsEnabled interface{} `json:"notifications_enabled"`
+}
